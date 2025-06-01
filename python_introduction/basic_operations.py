22d96b51f944c690ba263eac1a5b6dbd2edc1d1a
Edith Kosgey <kosgeyedith020@gmail.com>
2025-06-02 00:01:58 +0300

    Python files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+sub = number1 - number2
+prod = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Substraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {prod}")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,5 @@
+width = 5
+length = 10
+
+area = length * width
+print(f"The area of the rectangle is: {area}")
